0928e74711ca9d80bae9fcecf8ca979423f5dc61
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-09-23 11:42:22 -0500

    creacion de la primera anotacion

--- src/org/jhonatan/anotaciones/ejemplo/JsonAtribute.java
+++ src/org/jhonatan/anotaciones/ejemplo/JsonAtribute.java
@@ -1,5 +1,19 @@
 package org.jhonatan.anotaciones.ejemplo;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+//indicamos el target en donde se va aplicar esta anotacion
+@Target(ElementType.FIELD) //sobre atributos
+//sobre que contexto 
+@Retention(RetentionPolicy.RUNTIME)//en tiempo de ejecucion
 public @interface JsonAtribute {
+
+    /*atributos*/
+    String nombre() default "";
     
 }
