c21599c7502487235e79242be2b6ea6a6ba98aa1
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-09-23 11:44:21 -0500

    class producto

---
+++ src/org/jhonatan/anotaciones/ejemplo/models/Producto.java
@@ -0,0 +1,35 @@
+package org.jhonatan.anotaciones.ejemplo.models;
+
+import java.time.LocalDate;
+
+public class Producto {
+
+    private String nombre;
+    private long precio;
+    private LocalDate fecha;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(long precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+}
